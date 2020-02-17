9041b911006625c02762e1fa57937f35cb26b08c
Sadequs Haque <waseehaque@Sadequss-MBP.fios-router.home>
2020-02-17 00:00:17 -0500

    ReactJS working. Bootstrap added.

--- blood-donate-app/src/index.js
+++ blood-donate-app/src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Counter from './components/counter'
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const element = <h3>Hello World!</h3>;
+
+ReactDOM.render(<Counter />, document.getElementById('main-container'));
