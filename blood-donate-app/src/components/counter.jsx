59e54d5fa98523d8b25e8859f82f192e2b4e120b
Sadequs Haque <waseehaque@Sadequss-MBP.fios-router.home>
2020-02-17 00:19:52 -0500

    Accidentally deleted everythign

--- blood-donate-app/src/components/counter.jsx
+++ blood-donate-app/src/components/counter.jsx
@@ -17,7 +17,7 @@ class Counter extends Component {
 
     styles = {
 
-        fontSize: 30,
+        fontSize: 390,
         fontWeight: "bold"
 
     }
@@ -26,7 +26,7 @@ class Counter extends Component {
         return (
         <React.Fragment>
             <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
-            <button className= "btn btn-secondary btn-sm">Increase</button>
+            <button className= "btn btn-secondary btn-sm">Increment</button>
         </React.Fragment>
         );
     }
