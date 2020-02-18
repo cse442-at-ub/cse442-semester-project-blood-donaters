e2df6fa5eb58ea12bf061127ee6b434d7ea9b38c
SH <sadeqush@buffalo.edu>
2020-02-18 12:11:30 -0500

    #14 intregrated the React components according to convention.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Counter from './components/counter'
 import Login from './components/login'
+import App from './App'
 
 
-ReactDOM.render(<Login/>, document.getElementById('main-container'));
+ReactDOM.render(<App/>, document.getElementById('main-container'));
