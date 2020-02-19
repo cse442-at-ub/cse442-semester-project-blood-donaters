1a840aa53c210330f7c588a95df797c7ba7e3a3c
SH <sadeqush@buffalo.edu>
2020-02-19 15:14:09 -0500

    #14 more compliance with reactJS traditions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Counter from './components/counter'
 import Login from './components/login'
 import App from './App'
 
 
-ReactDOM.render(<App/>, document.getElementById('main-container'));
+ReactDOM.render(<App/>, document.getElementById('root'));
