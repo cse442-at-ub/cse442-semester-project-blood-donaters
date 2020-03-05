ca67105e16d15bfff5179c51462c6030cb7d5fa9
Philip Moraski <ptmorask@buffalo.edu>
2020-03-05 14:54:22 -0500

    #31. Removed some unused import statements from login.jsx.

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
@@ -7,9 +6,6 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 
 class Login extends Component {
