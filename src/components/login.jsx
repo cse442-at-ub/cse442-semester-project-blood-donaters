0b1f03aeff56c1ba14ba556b55eaece627b39909
Philip Moraski <ptmorask@buffalo.edu>
2020-03-05 18:06:04 -0500

    #31. Removed extraneous files and added package-lock.json to .gitignore.  Also added initial SQL code.

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -7,6 +7,7 @@ import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
+const mysql = require('mysql');
 
 class Login extends Component {
   constructor(props) {
@@ -15,7 +16,28 @@ class Login extends Component {
       username: "",
       password: ""
     };
-    console.log(props);
+  }
+
+  sqlQuery(query) {
+    let queriedRows;
+    
+    const connection = mysql.createConnection({
+      host: 'tethys',
+      user: 'ptmorask',
+      password: '50237054',
+      database: 'cse442_542_2020_spring_teamm_db'
+    });
+    connection.connect((err) => {
+      if (err) throw err;
+      console.log('Connected!');
+    });
+    connection.query(query, (err,rows) => {
+      if(err) throw err;
+    
+      queriedRows = rows;
+    });
+    connection.end();
+    return queriedRows;
   }
 
   render() {
@@ -28,68 +50,65 @@ class Login extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    this.props.userHasAuthenticated(true);
+    let rows = this.sqlQuery(`SELECT * FROM test WHERE username='${this.state.password}' && password='${this.state.username}';`);
+    console.log(rows);
   }
 
   loginForm() {
-    if (this.props.isAuthenticated) {
-      return <p>{"Signed in"}</p>;
-    }
-    else {
-      return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={this.paper}>
-          <form className={this.form} onSubmit={this.handleSubmit} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              onChange={e => this.setState({username: e.target.value})}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              onChange={e => this.setState({password: e.target.value})}
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={this.submit}
-              disabled={!this.validateForm()}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item>
-                <Link href="/signup" variant="body2">
-                  Don't have an account? Sign Up
-                </Link>
-              </Grid>
+    return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={this.paper}>
+        <form className={this.form} onSubmit={this.handleSubmit} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={e => this.setState({username: e.target.value})}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={e => this.setState({password: e.target.value})}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={this.submit}
+            disabled={!this.validateForm()}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link href="/signup" variant="body2">
+                Don't have an account? Sign Up
+              </Link>
             </Grid>
-          </form>
-        </div>
-      </Container>
-      )}
+          </Grid>
+        </form>
+      </div>
+    </Container>
+    )
   }
 }
 export default Login;
