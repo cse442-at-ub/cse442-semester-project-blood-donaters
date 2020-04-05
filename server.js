2d76cb03bee307e69e3d9c44758be3f023220e4d
Ethan Armbrust <ethanarm@buffalo.edu>
2020-04-05 17:33:13 -0400

    Server and client now properly communicate

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ let connection = mysql.createConnection({
   host: "tethys.cse.buffalo.edu",
   user: "ethanarm",
   password: "37694242",
-  database: "cse442_542_2020_spring_teamm_db"
+  database: "cse442_542_2020_spring_teamm_db",
 });
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.log(err);
   } else {
@@ -28,8 +28,8 @@ connection.connect(function(err) {
   }
 });
 
-app.get(`${dirname}/authenticate/:user/:pass`, async function(req, res) {
-  console.log(path.join(dirname, "test/testing"));
+app.get(`/authenticate/:user/:pass`, async function (req, res) {
+  console.log("app.get function called");
   let username = req.params.user;
   let password = req.params.pass;
   if (username == null || password == null) {
@@ -37,9 +37,7 @@ app.get(`${dirname}/authenticate/:user/:pass`, async function(req, res) {
   } else {
     let query = `SELECT * FROM test WHERE username='${username}' && password='${password}';`;
     let queriedRow;
-    await connection.query(query, function(err, rows, fields) {
-      if (err) throw err;
-
+    await connection.query(query, function (err, rows, fields) {
       queriedRow =
         rows.length === 1
           ? { username: rows[0].username, password: rows[0].password }
@@ -51,19 +49,20 @@ app.get(`${dirname}/authenticate/:user/:pass`, async function(req, res) {
 
 //get full database list
 
-app.get(`${dirname}/listdata`, async function(req, res) {
+app.get(`/listdata`, async function (req, res) {
   let query = `SELECT * FROM donor_list_test;`;
   let queriedRows;
-  await connection.query(query, function(err, rows, fields) {
-    if (err) throw err;
-
+  console.log("requested list data");
+  await connection.query(query, function (err, rows, fields) {
     queriedRows = rows;
+    console.log(queriedRows);
     res.json(queriedRows);
   });
 });
 
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(process.env.PORT || 3000);
+//app.listen(8080);
