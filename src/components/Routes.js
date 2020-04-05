319086651b54c641edeb6ff8a17c3942e69c5ab8
Ethan Armbrust <ethanarm@buffalo.edu>
2020-04-05 18:15:27 -0400

    working on converting donor list to jsx

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,22 +1,36 @@
-
 import React from "react";
-import AppliedRoute from './AppliedRoute';
+import AppliedRoute from "./AppliedRoute";
 import Login from "./login.jsx";
-import DonorList from './donors-list';
-import HomePage from './homepage';
-import SignUp from './formpage';
-import ProfilePage from './profilepage';
+import DonorList from "./donor-list.jsx";
+import HomePage from "./homepage";
+import SignUp from "./formpage";
+import ProfilePage from "./profilepage";
 import { Switch, Route } from "react-router-dom";
 
-export default function Routes({ appProps }) {  
-    return (
-      <Switch>
-        <AppliedRoute path="/" exact component={HomePage} appProps={appProps} />
-        <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
-        <AppliedRoute path="/signup" exact component={SignUp} appProps={appProps} />
-        <AppliedRoute path="/donors-list" exact component={DonorList} appProps={appProps} />
-        <AppliedRoute path="/profile" exact component={ProfilePage} appProps={appProps} />
-        <Route component={HomePage} />
-      </Switch>
-    );
-  }
+export default function Routes({ appProps }) {
+  return (
+    <Switch>
+      <AppliedRoute path="/" exact component={HomePage} appProps={appProps} />
+      <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+      <AppliedRoute
+        path="/signup"
+        exact
+        component={SignUp}
+        appProps={appProps}
+      />
+      <AppliedRoute
+        path="/donors-list"
+        exact
+        component={DonorList}
+        appProps={appProps}
+      />
+      <AppliedRoute
+        path="/profile"
+        exact
+        component={ProfilePage}
+        appProps={appProps}
+      />
+      <Route component={HomePage} />
+    </Switch>
+  );
+}
--- src/components/donor-list.jsx
+++ src/components/donor-list.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { withStyles, makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -8,123 +8,53 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 
-
 class DonorList extends Component {
-    state = { 
-        rows = [
-            createData(
-              "Abdur",
-              "Rahman",
-              "O Positive",
-              "Buffalo, New York",
-              "+123456789"
-            ),
-            createData(
-              "Steven",
-              "Smith",
-              "B Negative",
-              "Brooklyn, New York",
-              "+123456789"
-            ),
-            createData(
-              "Michael",
-              "Clarke",
-              "B Positive",
-              "Rochester, New York",
-              "+123456789"
-            ),
-            createData(
-              "Adam",
-              "Gilchrist",
-              "O Negative",
-              "Queens, New York",
-              "+123456789"
-            ),
-            createData(
-              "Matthew",
-              "Hayden",
-              "AB Positve",
-              "Manhattan, New York",
-              "+123456789"
-            ),
-            createData("Stuart", "Broad", "AB Negative", "Bronx, New York", "+123456789"),
-            createData(
-              "Shaun",
-              "Tait",
-              "B Negative",
-              "Long Island, New York",
-              "+123456789"
-            ),
-            createData(
-              "Morne",
-              "Morkel",
-              "B Negative",
-              "Binghamton, New York",
-              "+123456789"
-            )
-          ]
-     }
-    render() { 
-        return ( 
-            <div>
-      <h1 color="blue">Blood Donors List </h1>
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="customized table">
-          <TableHead>
-            <TableRow>
-              <StyledTableCell>First Name</StyledTableCell>
-              <StyledTableCell align="right">Last Name</StyledTableCell>
-              <StyledTableCell align="right">Blood Group</StyledTableCell>
-              <StyledTableCell align="right">Location</StyledTableCell>
-              <StyledTableCell align="right">Cell Phone No.</StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {state.rows.map(row => (
-              <StyledTableRow key={row.name}>
-                <StyledTableCell component="th" scope="row">
-                  {row.firstName}
-                </StyledTableCell>
-                <StyledTableCell align="right">{row.lastName}</StyledTableCell>
-                <StyledTableCell align="right">
-                  {row.bloodGroup}
-                </StyledTableCell>
-                <StyledTableCell align="right">{row.location}</StyledTableCell>
-                <StyledTableCell align="right">{row.cellphone}</StyledTableCell>
-              </StyledTableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
-         );
-    }
-    createData(firstName, lastName, bloodGroup, location, cellphone) {
-        return { firstName, lastName, bloodGroup, location, cellphone };
-    }
-    StyledTableCell = withStyles(theme => ({
-        head: {
-          backgroundColor: theme.palette.secondary.main,
-          color: theme.palette.common.white
+  constructor(props) {
+    super(props);
+    this.state = {
+      rows: [
+        {
+          firstName: "temp",
+          lastName: "templast",
+          bloodGroup: "C-",
+          location: "West Mexico",
+          cellphone: "123456789",
         },
-        body: {
-          fontSize: 14
-        }
-      }))(TableCell);
-
-      StyledTableRow = withStyles(theme => ({
-        root: {
-          "&:nth-of-type(odd)": {
-            backgroundColor: theme.palette.background.default
-          }
-        }
-      }))(TableRow);
-
-      useStyles = makeStyles({
-        table: {
-          minWidth: 700
-        }
-      });
+      ],
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1 color="blue">Blood Donors List </h1>
+        <TableContainer component={Paper}>
+          <Table aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <TableCell>First Name</TableCell>
+                <TableCell align="right">Last Name</TableCell>
+                <TableCell align="right">Blood Group</TableCell>
+                <TableCell align="right">Location</TableCell>
+                <TableCell align="right">Cell Phone No.</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.state.rows.map((row) => (
+                <TableRow key={row.name}>
+                  <TableCell component="th" scope="row">
+                    {row.firstName}
+                  </TableCell>
+                  <TableCell align="right">{row.lastName}</TableCell>
+                  <TableCell align="right">{row.bloodGroup}</TableCell>
+                  <TableCell align="right">{row.location}</TableCell>
+                  <TableCell align="right">{row.cellphone}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    );
+  }
 }
- 
 export default DonorList;
